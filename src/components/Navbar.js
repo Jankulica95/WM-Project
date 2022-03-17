73b95b086313b48afa217bcc8192c55b57955722
Nikola Jankovic <jankulica21@gmail.com>
2022-03-17 17:15:59 +0100

    Second

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
 import logo from "../LogoWM.jpg";
 import { links, social } from "./data";
 import { BiSearch } from "react-icons/bi";
+import { BsHouseDoorFill } from "react-icons/bs";
+import { AiFillQuestionCircle } from "react-icons/ai";
+import { FaGraduationCap } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FiAlignJustify } from "react-icons/fi";
+
 import "./Navbar.css";
 
 const Navbar = () => {
@@ -9,7 +15,33 @@ const Navbar = () => {
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" className="logo" />
-          <button className="nav-toggle">meni ikonica</button>
+          <div className="nav-toggle">
+            <FiAlignJustify />
+          </div>
+          <div className="header-list">
+            <ul className="header-links-s">
+              <li>
+                <a href="#" id="link-s">
+                  <BsHouseDoorFill />
+                </a>
+              </li>
+              <li>
+                <a href="#" id="link-s">
+                  <AiFillQuestionCircle />
+                </a>
+              </li>
+              <li>
+                <a href="#" id="link-s">
+                  <FaGraduationCap />
+                </a>
+              </li>
+              <li>
+                <a href="#" id="link-s">
+                  <IoIosMail />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="links-container">
           <ul className="links">
